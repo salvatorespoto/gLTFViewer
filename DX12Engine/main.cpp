bbea9dd8d47aed718ef01a8f86b68c9dc3cc63af
Salvatore Francesco Spoto <salvatore.spoto@gmail.com>
2020-08-20 17:19:07 +0200

    Remove comments from main.cpp

--- DX12Engine/main.cpp
+++ DX12Engine/main.cpp
@@ -1,8 +1,3 @@
-// dear imgui: standalone example application for DirectX 12
-// If you are new to dear imgui, see examples/README.txt and documentation at the top of imgui.cpp.
-// FIXME: 64-bit only for now! (Because sizeof(ImTextureId) == sizeof(void*))
-
-#define _CRT_SECURE_NO_WARNINGS
 #include "WindowApp.h"
 
 int CALLBACK wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR lpCmdLine, int nCmdShow)
