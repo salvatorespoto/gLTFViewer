db903da3bf1286a6a8f53c92f997023c8133959e
Salvatore Francesco Spoto <sspoto@34bigthings.com>
2022-09-25 18:26:53 +0200

    Fix winmain annotations

--- DX12Engine/main.cpp
+++ DX12Engine/main.cpp
@@ -3,7 +3,8 @@
 
 #include "ViewerApp.h"
 
-int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ PWSTR lpCmdLine, _In_ int nCmdShow)
+_Use_decl_annotations_
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR, int nCmdShow)
 {
     ViewerApp viewer(hInstance);
     viewer.Run();
